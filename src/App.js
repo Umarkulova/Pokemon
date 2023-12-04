c4a2232a709afd8e59b1d0832d779ad3bf897671
Asem Umarkulova <umarkulovaasem@gmail.com>
2023-12-04 11:55:49 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import Main from './Components/Main';
+import './Components/style.css'
+import {useEffect, useState} from "react"
 import './App.css';
+import axios from "axios"
+
+
 
 function App() {
+const [nameList, setNameList] = useState([])
+const [search, setSearch] = useState("")
+
+
+
+useEffect(()=> {
+  axios.get("https://pokeapi.co/api/v2/pokemon?limit=500")
+  .then((response)=>{setNameList(response.data.results)})
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Main/>
+    </>
+  )
 }
 
 export default App;
